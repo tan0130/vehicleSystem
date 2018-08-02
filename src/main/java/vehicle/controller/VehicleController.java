c0522b1ea70c7b907576ef709c8d49f10e64d236
tanjinming <1311230692@qq.com>
2018-08-02 17:47:39 +0800

    添加远程控制和远程升级选项卡，上传文件部分未实现

--- src/main/java/vehicle/controller/VehicleController.java
+++ src/main/java/vehicle/controller/VehicleController.java
@@ -1,19 +1,27 @@
 package vehicle.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.stage.FileChooser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import vehicle.entity.Vehicle;
 import vehicle.service.VehicleService;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.swing.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.*;
 
 /**
  * create by tan on 2018/5/14
@@ -86,14 +94,116 @@ public class VehicleController {
         return "page/motorData";
     }
 
-    @RequestMapping(value = "/toVoltagePage")
+    @RequestMapping(value = "/toControlPage")
     public String toVoltagePage() {
-        return "page/voltageData";
+        return "page/remoteControl";
     }
 
-    @RequestMapping(value = "/toTemperaturePage")
+    @RequestMapping(value = "/toUpdatePage")
     public String toTemperaturePage() {
-        return "page/temperatureData";
+        return "page/remoteUpdate";
+    }
+
+    // test 远程控制发送指令
+    @RequestMapping(value = "/sendCommandCode")
+    @ResponseBody
+    public String sendCommandCode() {
+        try {
+            Thread.currentThread().sleep(5000);
+//            System.out.print("返回 true");
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "fail";
+    }
+
+    // 保存历史记录信息到本地文档中
+    @RequestMapping(value = "/saveHistoryText")
+    @ResponseBody
+    public String saveHistoryText(String historyText) {
+        System.out.print("保存记录");
+        try {
+            JFileChooser jfileChooser = new JFileChooser();
+            jfileChooser.setMultiSelectionEnabled(false); // 不支持多选
+            jfileChooser.setFileSelectionMode(JFileChooser.SAVE_DIALOG | JFileChooser.DIRECTORIES_ONLY);
+            jfileChooser.setDialogTitle("选择目标文件夹");
+            if (jfileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                File file = jfileChooser.getSelectedFile();
+                String filePath = file.getPath();
+                PrintWriter printWriter = new PrintWriter(filePath + "\\" +  new Date().getTime() + "-history_text.txt");
+                printWriter.println(historyText);
+                printWriter.close();
+
+            } else {
+                return "fail";
+            }
+
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "false";
+
+    }
+
+    // 打开文件选择框
+    @RequestMapping(value = "/selectFile")
+    @ResponseBody
+    public String selectFile() {
+        System.out.print("选择文件");
+        File file = null;
+        String filePath = null;
+        try {
+
+            JFileChooser jfileChooser = new JFileChooser();
+            jfileChooser.setMultiSelectionEnabled(false); // 不支持多选
+            //jfileChooser.setFileSelectionMode(JFileChooser.SAVE_DIALOG | JFileChooser.DIRECTORIES_ONLY);
+            jfileChooser.setDialogTitle("选择目标文件");
+            if (jfileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                file = jfileChooser.getSelectedFile();
+                filePath = file.getPath();
+            } else {
+                return "fail";
+            }
+            System.out.print("文件路径为：" + filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return filePath;
+    }
+
+    // 上传文件
+    @RequestMapping(value = "/upload")
+    @ResponseBody
+    public String uploadFile(String filePath, HttpServletRequest request, MultipartFile multipartFile) {
+        System.out.print("上传文件");
+        try {
+            // 将当前上下文初始化给 CommonsMultipartResolver (多部分解析器)
+            CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+                    request.getSession().getServletContext());
+            // 检查 form 中是否有 enctype="multipart/form-data"
+            if (multipartResolver.isMultipart(request)) {
+                // 将 request 变成多部分 request
+                MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
+                // 获取 multiRequest 中所有的文件名
+                Iterator iter = multipartHttpServletRequest.getFileNames();
+//                while (iter.hasNext()) {
+//                    // 一次遍历所有文件
+                   // MultipartFile multipartFile = multipartHttpServletRequest.getFile(filePath);
+//                }
+                //File file = new File(filePath);
+                String path = "D:/vehicleUpload" + multipartFile.getOriginalFilename();
+                File file = new File(path);
+                // 上传
+                multipartFile.transferTo(file);
+                return "success";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "fail";
     }
 
     // 报文信息查询
